0289981211310be8d7a46c1f1c86d928859a9c2a
dylansun614 <ysun0948@gmail.com>
2026-07-12 20:49:34 +1000

    feat: set up cart context provider

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,20 @@
+import { createContext, useState } from "react"
+
+const CartContext = createContext()
+
+export function CartProvider({ children }) {
+  const [cartItems, setCartItems] = useState([])
+
+  return (
+    <CartContext.Provider
+      value={{
+        cartItems,
+        setCartItems,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  )
+}
+
+export default CartContext
